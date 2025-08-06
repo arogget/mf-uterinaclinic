a66950b567ce73be3a1194a0d7d65f03fb4ad860
unknown <a.rogget@gmail.com>
2025-08-06 19:36:12 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function mostrarAlerta() {
+  alert("Gracias por tu interés. Muy pronto habilitaremos la reserva online.");
+}
+
+function enviarFormulario(event) {
+  event.preventDefault();
+  alert("Mensaje enviado. Nos pondremos en contacto contigo pronto.");
+}
